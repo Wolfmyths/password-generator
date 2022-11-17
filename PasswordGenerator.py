6fe6840220402792c37740e2b1cd622eb9445d15
Wolfmyths <118236986+Wolfmyths@users.noreply.github.com>
2022-11-17 03:33:23 -0500

    Update PasswordGenerator.py
    - Added more comments
    - Fixed a bug: The first letter in words from passphrases wouldn't be capitalized

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,6 @@
 import random
 import requests
+import urllib3 # Added so py installer could find the correct modules
 class password:
     def __init__(self, strength='strong', length=0, capital='true'):
         self.strength = strength
@@ -21,11 +22,11 @@ class password:
 
         for i in range(length):
 
-            charChoice = self.charChoice[random.randint(0, 2)] if self.strength == 'strong' else self.charChoice[random.randint(0, 1)] #If the strength is set to strong it will include the index value for symbols
-            i = charChoice[random.randint(0, (len(charChoice) - 1))] # subtracts 1 from the list length to compensate lists in python being starting sub 0
+            charChoice = self.charChoice[random.randint(0, 2)] if self.strength == 'strong' else self.charChoice[random.randint(0, 1)] # If the strength is set to strong it will include the index value for symbols
+            i = charChoice[random.randint(0, (len(charChoice) - 1))] # Subtracts 1 from the list length to compensate lists in python being starting sub 0
 
-            if self.capital == 'true' and i in self.alphabet: #If statement checks if the character can be capitalised
-                if random.randint(0, 1) == 1: #Coin flip
+            if self.capital == 'true' and i in self.alphabet: # If statement checks if the character can be capitalised
+                if random.randint(0, 1) == 1: # Coin flip
                     i = i.upper()
             
             password += i
@@ -33,7 +34,7 @@ class password:
         return password
 
 class passphrase:
-    def __init__(self, length=0, capitalize='false'):
+    def __init__(self, length=0, capitalize=''):
         self.length = length
         self.capitalize = capitalize
     
@@ -47,12 +48,12 @@ class passphrase:
             return f'The length "{length}" is too high, use 250 or below'
         
         for i in range(length):
-            word = requests.get('https://random-word-api.herokuapp.com/word')
+            word = requests.get('https://random-word-api.herokuapp.com/word') # Access the random word API
             word = word.text
-            word = word[2:-2]
+            word = word[2:-2] # word.text has brackets and quotations around it ( ["Example"] ) , this line removes those
             if self.capitalize == 'true':
-                word.title()
-            if password == '':
+                word = word.title()
+            if password == '': # If statement determines if the word being added to the passphrase is the first word
                 password += word
             else:
                 password += f' {word}'
