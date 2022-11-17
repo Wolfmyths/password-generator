30daf6751cc74c8c03d4af62d608419607eb9e2a
Wolfmyths <118236986+Wolfmyths@users.noreply.github.com>
2022-11-17 03:28:09 -0500

    Update main.py
    Rewrote code in favor of new UI implementation

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import PasswordGenerator
-
+import GUI
 if __name__ == '__main__':
-    word_or_phrase = input('Generate password or passphrase? ')
-    if word_or_phrase == 'password':
-        args = input(' strong/weak | length (max 250) (0 for random)| Includes capital letters (True/False, default true): ')
-        args = args.lower()
-        args = args.split()
-        generate = PasswordGenerator.password(args[0], args[1], args[2])
-        print(generate.get_pass())
-    elif word_or_phrase == 'passphrase':
-        args = input(' word amount (max 50) (0 for random) | Capitalize first letter? (True/False, default false): ')
-        args = args.lower()
-        args = args.split()
-        generate = PasswordGenerator.passphrase(args[0], args[1])
-        print(generate.get_phrase())
-    else:
-        print(f'Invalid choice {word_or_phrase}')
+
+    GUI.mainGUI()
